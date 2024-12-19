16e2f33d10ef10d0e9bbf213c8328837890cd666
Roman <roman@mbp14.local>
2024-12-19 22:59:08 +0300

    Added detekt plugin to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,18 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.0"
+
+}
+
+detekt {
+    config = files("detekt.yml")
+    parallel = true
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
